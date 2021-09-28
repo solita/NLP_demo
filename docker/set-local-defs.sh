369c48c166bfdc8d2a08ab0be9ff02f842b955f0
kariantila <62381425+kariantila@users.noreply.github.com>
2021-09-28 16:27:18 +0300

    IMDb example notebook updated.

--- docker/set-local-defs.sh
+++ docker/set-local-defs.sh
@@ -3,5 +3,6 @@
 LOCAL_KERNEL_NAME=$(uname -s)
 
 if [ ${LOCAL_KERNEL_NAME}='Mac' ]; then
-	export SHARE_DATA_PATH='/Users/kari.antila/Projects/NLP_demo/share'
+	export SHARE_DATA_PATH='/Users/'$USER'/Projects/NLP_demo/share'
+	export GIT_REPO_PATH='/Users/'$USER'/GitHub/NLP_demo'
 fi
