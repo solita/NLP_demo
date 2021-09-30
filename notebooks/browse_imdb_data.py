d43ba7fb81b00d5332e69dc4ed8347b023c4836f
kariantila <62381425+kariantila@users.noreply.github.com>
2021-09-30 16:58:24 +0300

    Raw sentiment text files read

--- notebooks/browse_imdb_data.py
+++ notebooks/browse_imdb_data.py
@@ -9,4 +9,13 @@ file_name = os.path.join(data_dir, 'aclImdb/train/urls_neg.txt')
 
 df_train_neg_urls = pd.read_csv(file_name, sep='\s+', header=None)
 
-df_train_neg_urls.head()
+print('Number of urls (train, neg): %d' % len(df_train_neg_urls))
+
+file_list = []
+
+for root, dirs, files in os.walk(os.path.join(data_dir, 'aclImdb/train/neg')):
+    for file_name in files:
+        if file_name.endswith('.txt'):
+            file_list.append(file_name)
+
+print('Number of urls (train, neg): %d' % len(file_list))
